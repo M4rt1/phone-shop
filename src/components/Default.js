9f56a50004f10fc451c8c0acf5c089f4c40d70d3
MSLB <msbulkowski@protonmail.com>
2021-05-28 19:38:43 +0200

    finished default page
    mobile and desktop view

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,7 +3,9 @@ import '../styles/Default.css';
 function Default() {
     return (
         <div className='default-container'>
-
+            <p>
+                We provide information on top smartphones and the most important technological terminologies.
+            </p>
         </div>
     )
 }
